cebe54f8e2bcb41278ea53635943f0c4aedc66a7
Arinze Mmere <frankari101@gmail.com>
2026-06-29 03:41:09 -0700

    Update local.tf

--- local.tf
+++ local.tf
@@ -1,5 +1,4 @@
   # User data template for Secondary instance
-  secondary_user_data = <<-EOF
     #!/bin/bash
     apt-get update -y
     apt-get install -y apache2
@@ -7,5 +6,5 @@
     systemctl enable apache2
     echo "<h1>Secondary VPC Instance - ${var.secondary}</h1>" > /var/www/html/index.html
     echo "<p>Private IP: $(hostname -I)</p>" >> /var/www/html/index.html
-  EOF
+  
 }
